746992d066c180ba8efbb93899c9e1b75afe7111
duttprem <premdutt083@gmail.com>
2023-04-28 01:09:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,214 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="table-responsive">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Prem Dutt</td>
+            <td>premdutt083@gmail.com</td>
+            <td>8958070079</td>
+            <td>Mathura</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Krishana</td>
+            <td>krishan@gmail.com</td>
+            <td>8987878789</td>
+            <td>Hathras</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Boby</td>
+            <td>Boby@gmail.com</td>
+            <td>7878767676</td>
+            <td>Aligarh</td>
+          </tr>
+
+          <tr>
+            <td>4</td>
+            <td>Rohit</td>
+            <td>rohit@gmail.com</td>
+            <td>7878767676</td>
+            <td>Mathura</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Karan</td>
+            <td>abcd@gmail.com</td>
+            <td>7878767676</td>
+            <td>Aligarh</td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>Rohan</td>
+            <td>Rohan@gmail.com</td>
+            <td>7878767676</td>
+            <td>Agra</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>Vilash</td>
+            <td>vilash@gmail.com</td>
+            <td>7878767676</td>
+            <td>Aligarh</td>
+          </tr>
+          <tr>
+            <td>8</td>
+            <td>Prem</td>
+            <td>premexample.com</td>
+            <td>7878767676</td>
+            <td>Mathura</td>
+          </tr>
+          <tr>
+            <td>9</td>
+            <td>Yash</td>
+            <td>Yash@gmail.com</td>
+            <td>7878767676</td>
+            <td>Aligarh</td>
+          </tr>
+          <tr>
+            <td>10</td>
+            <td>Pavan</td>
+            <td>pavan@gmail.com</td>
+            <td>7878767676</td>
+            <td>Aligarh</td>
+          </tr>
+
+          <tr>
+            <td>11</td>
+            <td>Shivam</td>
+            <td>shivan@gmail.com</td>
+            <td>7878767676</td>
+            <td>Aligarh</td>
+          </tr>
+
+          <tr>
+            <td>12</td>
+            <td>Vishal</td>
+            <td>vishal123@gmail.com</td>
+            <td>7878767676</td>
+            <td>Aligarh</td>
+          </tr>
+
+          <tr>
+            <td>13</td>
+            <td>Pavan</td>
+            <td>pavan@gmail.com</td>
+            <td>7878767676</td>
+            <td>Aligarh</td>
+          </tr>
+
+          <tr>
+            <td>14</td>
+            <td>Vilash</td>
+            <td>vilash@gmail.com</td>
+            <td>7878767676</td>
+            <td>Noida</td>
+          </tr>
+
+          <tr>
+            <td>15</td>
+            <td>Vilash</td>
+            <td>vilash@gmail.com</td>
+            <td>7878767676</td>
+            <td>Mathura</td>
+          </tr>
+
+          <tr>
+            <td>16</td>
+            <td>Vilash</td>
+            <td>vilash@gmail.com</td>
+            <td>7878767676</td>
+            <td>Aligarh</td>
+          </tr>
+
+          <tr>
+            <td>17</td>
+            <td>Pavan</td>
+            <td>pavan@gmail.com</td>
+            <td>7878767676</td>
+            <td>Aligarh</td>
+          </tr>
+
+          <tr>
+            <td>18</td>
+            <td>Pratap</td>
+            <td>pavan@gmail.com</td>
+            <td>7878767676</td>
+            <td>Mathura</td>
+          </tr>
+
+          <tr>
+            <td>19</td>
+            <td>Pavan</td>
+            <td>pavan@gmail.com</td>
+            <td>7878767676</td>
+            <td>Aligarh</td>
+          </tr>
+
+          <tr>
+            <td>20</td>
+            <td>Rajat Sharma</td>
+            <td>pavan@gmail.com</td>
+            <td>7878767676</td>
+            <td>Aligarh</td>
+          </tr>
+
+          <tr>
+            <td>21</td>
+            <td>Rohan</td>
+            <td>Rohan@gmail.com</td>
+            <td>7878767676</td>
+            <td>Agra</td>
+          </tr>
+
+          <tr>
+            <td>22</td>
+            <td>Pratap</td>
+            <td>pavan@gmail.com</td>
+            <td>7878767676</td>
+            <td>Mathura</td>
+          </tr>
+
+          <tr>
+            <td>23</td>
+            <td>Himanshu Kaushal</td>
+            <td>pavan@gmail.com</td>
+            <td>7878767676</td>
+            <td>Mathura</td>
+          </tr>
+
+          <tr>
+            <td>24</td>
+            <td>Shivam Chaudhary</td>
+            <td>pavan@gmail.com</td>
+            <td>7878767676</td>
+            <td>Delhi</td>
+          </tr>
+          <tr>
+            <td>25</td>
+            <td>Suraj Kumar</td>
+            <td>pavan@gmail.com</td>
+            <td>7878767676</td>
+            <td>Gurugram</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
